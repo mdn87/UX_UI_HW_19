eab3d4f735564c6f9e5a0078dd2e93cf140c4174
mdn87 <matthew.d.newman@gmail.com>
2022-11-01 23:18:20 -0400

    stupid jquery cant do anything good

--- js/index.js
+++ js/index.js
@@ -1,5 +1,74 @@
-// Use jQuery to target the <h1> tag and make it red.
-$("h1").css("color", "red");
+$("#proj1").hover(function(){
+        console.log("Mouse Enter");
+    }, function() {
+        console.log("Mouse Leave");
+});
+$("#proj2").click(function(){
+    $(this).animate({
+      opacity: '0.5'
+    },500
+    );
+  }); 
+$(document).ready(function() {   
+    function animateCloud1() {
+        $('#cloud1').animate({
+            'right':'400px'
+        }
+            ,16000, 'linear'
+        )
+        .animate(
+            {'right':'1000px'}
+            ,1
+            ,animateCloud1
+        ); 
+    }
 
-// Use jQuery to target the addMe ID and change its HTML to Hello.
-$("#addMe").html('Hello');
+    animateCloud1();
+}); 
+$(document).ready(function() {   
+    function animateCloud2() {
+        $('#cloud2').animate({
+            'right':'400px'
+        }
+            ,6000, 'linear'
+        )
+        .animate(
+            {'right':'1000px'}
+            ,1, 'linear'
+            ,animateCloud2
+        ); 
+    }
+
+    animateCloud2();
+}); 
+
+//jQuery does not have keyframes built in
+$.keyframe.define([{
+  name:'orbitPos',
+  '0%': {
+    'margin-left':'0px'
+  },
+  '50%': {
+    'margin-left':'600px'
+  },
+  '100%': {
+    'margin-left':'0px'
+  }
+}, {
+  name:'orbitScale',
+  from: {
+    'transform':'rotate(90deg)'
+  },
+  to: {
+    'transform':'rotate(450deg)'
+  }
+}]);
+$('#orbit').playKeyframe({
+  name:'orbitPos',
+  duration:"3s",
+  timingFunction:'ease',
+  iterationCount:'infinite',
+  direction:'normal',
+  fillMode:'forwards',
+  complete: increment
+});
