b5c356a0e870be1ed0c7bccf0a79605bcd42536f
mdn87 <matthew.d.newman@gmail.com>
2022-11-12 22:05:18 -0500

    moon animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,57 @@
+let canvas = document.getElementById("cart");
+let ctx = canvas.getContext("2d");
+let width = (canvas.width = window.innerWidth);
+let height = (canvas.height = window.innerHeight);
+
+let tail;
+
+let cart = document.getElementById("the-cart");
+
+let last = Date.now();
+
+class Tail {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.tx = 1;
+    this.ty = 1;
+    this.lastx = x;
+    this.lasty = x;
+  }
+
+  move(tx, ty) {
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(tx, ty);
+    ctx.stroke();
+    this.x = tx;
+    this.y = ty;
+    this.tx = tx;
+    this.ty = ty;
+  }
+}
+
+let startAnimation = () => {
+  let newOffset = getOffset(cart);
+  //tail.move(newOffset.left, newOffset.top);
+  window.requestAnimationFrame(startAnimation);
+};
+
+window.onload = function () {
+  let offset = getOffset(cart);
+  //tail = new Tail(offset.left, offset.top);
+  window.requestAnimationFrame(startAnimation);
+};
+
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX + 25,
+    top: rect.top + window.scrollY + 25,
+  };
+}
+
+
 // Hover projects in navigation
 $(document).ready(function() {
   $('.nav').hover(function(){
